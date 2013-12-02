4b2749b54d6dbe5a26068a0e9afd922f53bf964a
AJ <aj@govwizely.com>
2013-12-02 07:06:03 -0500

    update

---
+++ _site/javascripts/ex2.js
@@ -0,0 +1,12 @@
+var ex02 = {
+	template:'div.hello',
+	data:{ who:'Hello Wrrrld' },
+	directive:{ 'span.who':'who' }
+};
+
+var ex03 = {
+	template:'div.hello',
+	data:{ who:'Hello Wrrrld' },
+	directive:{ 'span.id':'id' }
+};
+
---
+++ javascripts/ex2.js
@@ -0,0 +1,7 @@
+var ex02 = {
+	template:'div.hello',
+	data:{ who:'Hello Wrrrld' },
+	directive:{ 'span.who':'who' }
+};
+
+
